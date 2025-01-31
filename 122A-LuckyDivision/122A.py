afa657962a20c03bf4653d3a53b1b0e63a10eae9
Aishwarya Iyer <90432405+ryaready@users.noreply.github.com>
2025-01-31 10:43:54 +0800

    done

---
+++ 122A-LuckyDivision/122A.py
@@ -0,0 +1,27 @@
+n = int(input())
+
+if n%4 == 0:
+    print('YES')
+elif n%7 == 0: 
+    print('YES')
+elif n%47 == 0:
+    print('YES')
+elif n%74 == 0: 
+    print('YES')
+elif n%444 == 0:
+    print('YES')
+elif n%447 == 0: 
+    print('YES')
+elif n%474 == 0: 
+    print('YES')
+elif n%477 == 0: 
+    print('YES')
+elif n%744 == 0: 
+    print('YES')
+elif n%747 == 0: 
+    print('YES')
+elif n%777 == 0: 
+    print('YES')
+else: 
+    print('NO')
+
